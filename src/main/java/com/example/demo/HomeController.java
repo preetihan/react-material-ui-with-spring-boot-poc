7239224b95c11016cdd814c2c029a3d40bfb06b2
preetihan <preeti.han@itmx.co.th>
2020-02-20 11:57:25 +0700

    edit route path controller

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,19 +1,14 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/")
-	public String index() {
-		return "index";
-	}
-
-	@RequestMapping(value = "/dashboard")
-	public String dashboard() {
-		return "index";
+	@GetMapping(value = "/**/{path:[^\\.]*}")
+	public String forward() {
+		return "forward:/";
 	}
 
 }
