4df82c445c51c28f5fb9efbb8d3970fd90421168
preetihan <preeti.han@itmx.co.th>
2020-02-20 15:50:47 +0700

    add axios for call api

--- src/main/js/page/Dashboard.js
+++ src/main/js/page/Dashboard.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ChartCard from '../component/ChartCard';
 import CustomTable from '../component/CustomPaginationActionsTable';
 import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
 
 class Dashboard extends React.Component {
+
+    state = {
+        messages: []
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8080/api/test')
+          .then(function (response) {
+            console.log(response.data);
+        });
+    }
+
     render() {
         return (
             <Grid container spacing={3}>
